c7a664b0f447c77e4e18b1b85a45deefc0c20a84
elupu <elupu@eyal-ub16-vm>
2017-12-17 23:13:11 +0000

    Adding basic authentication

---
+++ src/main/java/com/eyallupu/blog/kmsaad/config/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+
+package com.eyallupu.blog.kmsaad.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("eyal1").password("eyal1-pass").roles("USER").and().withUser("eyal2")
+				.password("eyal2-pass").roles("USER");
+	}
+
+}
